197362c4806f2665c1504d6158653b7540d4b11f
Gabriel Santos <gabrielsantos.dev@outlook.com>
2024-03-18 13:40:09 -0300

    ✨ feat: Create error component

---
+++ src/app/core/components/error/error.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-error',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './error.component.html',
+})
+export class ErrorComponent {
+  @Input({required: true}) errorTitle: string = '';
+  @Input() description: string = '';
+}
--- src/app/modules/profile-search/components/result-card/result-card.component.ts
+++ src/app/modules/profile-search/components/result-card/result-card.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { GithubProfile } from '../../../../core/models/github-profile.model';
 import { RouterLink } from '@angular/router';
+import { ErrorComponent } from '../../../../core/components/error/error.component';
 
 @Component({
   selector: 'app-result-card',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ErrorComponent],
   templateUrl: './result-card.component.html',
 })
 export class ResultCardComponent {
