82084820f69c0be106893e347254e6ed4c227969
Gabriel Santos <gabrielsantos.dev@outlook.com>
2024-03-19 18:08:53 -0300

    ✨ feat: add input validation

--- src/app/modules/profile-search/components/search-card/search-card.component.ts
+++ src/app/modules/profile-search/components/search-card/search-card.component.ts
@@ -2,21 +2,25 @@ import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { GithubProfileService } from '../../../../core/services/github-profile.service';
 import { FormsModule } from '@angular/forms';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-search-card',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, NgClass],
   templateUrl: './search-card.component.html'
 })
 export class SearchCardComponent {
   userName: string = '';
+  errorMsg: string = '';
   private router = inject(Router);
 
   onSubmit(event: Event) {
     event.preventDefault();
     if (this.userName.length > 0) {
       this.router.navigate([`users/${this.userName}`]);
+    } else {
+      this.errorMsg = 'Informe um nome de usuário válido.'
     }
   }
 }
