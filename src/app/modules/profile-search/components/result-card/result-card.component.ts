fff9ead3dbf3d55caeaf0f061d2f6795e4931b55
Gabriel Santos <gabrielsantos.dev@outlook.com>
2024-03-19 20:01:30 -0300

    🐞 fix: correct githubProfile

--- src/app/modules/profile-search/components/result-card/result-card.component.ts
+++ src/app/modules/profile-search/components/result-card/result-card.component.ts
@@ -10,7 +10,19 @@ import { ErrorComponent } from '../../../../shared/components/error/error.compon
   templateUrl: './result-card.component.html',
 })
 export class ResultCardComponent {
-  @Input({ required: true }) githubProfile: GithubProfile | null = null;
+  @Input({ required: true }) githubProfile: GithubProfile = {
+    login: '',
+    avatar_url: '',
+    name: '',
+    company: '',
+    location: '',
+    bio: '',
+    html_url: '',
+    public_repos: 0,
+    followers: 0,
+    following: 0,
+    created_at: new Date()
+  };
   @Input({ required: true }) username: string = '';
 
 }
