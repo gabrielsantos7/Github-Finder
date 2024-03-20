2b8b5470d4b7a2f23b0314611511f5bcd8b68fe2
Gabriel Santos <gabrielsantos.dev@outlook.com>
2024-03-20 19:47:34 -0300

    ✨ feat: create 404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { UserSearchPage } from './modules/profile-search/pages/user-search/user-search.page';
 import { UserReposPage } from './modules/profile-search/pages/user-repos/user-repos.page';
 import { UserProfilePage } from './modules/profile-search/pages/user-profile/user-profile.page';
+import { NotFoundPage } from './modules/profile-search/pages/not-found/not-found.page';
 
 export const routes: Routes = [
   { path: '', component: UserSearchPage },
   { path: 'users/:username', component: UserProfilePage },
-  { path: 'users/:username/repos', component: UserReposPage }
+  { path: 'users/:username/repos', component: UserReposPage },
+  { path: '**', component: NotFoundPage }
 ];
